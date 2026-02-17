757af501b2e295dfe595b4398f0844f7b759fdfd
Patrick B Warren <patrickbwarren@gmail.com>
2026-02-17 00:25:17 +0000

    will be replaced by proper makefile

---
+++ make_all_figures.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo ./fig1_make.py -o ../fig1.pdf
+echo ./fig2_make.py -o ../fig2.pdf
+echo ./fig3_make.py vardp100k.ods ../fig3.pdf
+echo ./fig4_make.py -o ../fig4.pdf
+
+echo ./figS1_make.py data/vardp100k.dat.gz -j --dpi 300 -o ../figS1.png
+echo ./figS2_make.py varrc100k.ods -o ../figS2.pdf
+echo ./figS3_make.py poremsteps.ods -o ../figS3.pdf
+echo ./figS4_make.py porerc10k.ods -o ../figS4.pdf
+
