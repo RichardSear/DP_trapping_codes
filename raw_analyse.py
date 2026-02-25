31b67e39e2c6e56b3977fdb3642f130a0c74d660
Patrick B Warren <patrickbwarren@gmail.com>
2026-02-25 22:46:21 +0000

    updating scripts

--- raw_analyse.py
+++ raw_analyse.py
@@ -12,6 +12,7 @@ import pandas as pd
 parser = argparse.ArgumentParser(description='compile raw BD data to a spreadsheet')
 parser.add_argument('dataset', help='raw input data file, eg *.dat.gz')
 parser.add_argument('-c', '--column', default='Dp', help='select data column, default Dp')
+parser.add_argument('-f', '--filter', default=None, help='filter on a data column, default none')
 parser.add_argument('-o', '--output', help='output compiled data to a spreadsheet, eg .ods, .xlsx')
 args = parser.parse_args()
 
@@ -30,6 +31,15 @@ col = args.column
 
 df = pd.read_csv(args.dataset, sep='\t', names=schema.keys(), dtype=schema)
 df.sort_values([col, 'Q', 'traj'], inplace=True)
+nrec = df.shape[0]
+
+if args.filter is not None:
+    cut = pd.Series(False, index=df.index)
+    for filt in args.filter.split(','):
+        filt_col = filt.split('=')[0]
+        filt_val = eval(filt.split('=')[1])
+        cut = cut | (df[filt_col] == filt_val)
+    df = df[cut]
 
 df2 = df[['Q', col, 'block', 'Δr2']].groupby(['Q', col, 'block']).mean() # calculate mean square displacement per block
 
@@ -41,11 +51,14 @@ ser2 = df2[['Q', col, 'RMSD']].groupby(['Q', col]).sem()['RMSD'].rename('std_err
 
 df3 = pd.concat([ser1, ser2], axis=1).reset_index() # compile these into a new dataframe, pulling index (Q) back to a column
 
+vals = ', '.join([str(val) for val in df[col].unique()])
+summary = f'Data from {args.dataset} ({df.shape[0]}/{nrec} records) {col} = {vals}'
+
 if args.output:
     with pd.ExcelWriter(args.output) as writer:
         for val in df3[col].unique():
             df3[df3[col] == val].drop(col, axis=1).to_excel(writer, sheet_name=f'{col}={val}', index=False)
-    vals = ', '.join([str(val) for val in df[col].unique()])
-    print(f'Data from {args.dataset} ({df.shape[0]} records) {col} = {vals} written to', args.output)
+    print(summary, 'written to', args.output)
 else:
+    print(summary)
     print(df3)
